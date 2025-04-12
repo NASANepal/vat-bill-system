af59e11606473b1bfcfe7d92a2e2c97b367f2005
NASANepal <u36store@gmail.com>
2025-04-12 21:26:45 +0545

    Added App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import InvoiceDemo from './InvoiceDemo'; // Assuming InvoiceDemo is the previous file you worked on
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the Invoice System</h1>
+      <InvoiceDemo />
+    </div>
+  );
+}
+
+export default App;
